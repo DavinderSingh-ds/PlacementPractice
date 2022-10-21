e0f6fbf1d44c39dc0b0d156c3962e0b17c15ad22
DavinderSingh-ds <davindersingh00743@gmail.com>
2022-10-21 19:35:21 +0530

    ArrayString

---
+++ Strings/arrayString.java
@@ -0,0 +1,7 @@
+class arrayString {
+    public static void main(String[] args) {
+        char ch[] = {'j','a','c','q','u'};
+        String s =  new String(ch);
+        System.out.println(s);
+    }
+}
